449340c7dfb7fb59315289c8c0bfb75f497cbeb5
Yosua <yosuablancodiaz6@gmail.com>
2018-06-12 15:07:46 -0600

    Nuevas clases y frames
    ventan principal del cliente, clase utilidades y el xml con los tags de yosi y mios

---
+++ Client_socket.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iiiprogra;
+
+import java.io.*;
+import java.net.*;
+/**
+ *
+ * @author josca
+ */
+public class Client_socket {
+    
+    private Socket cliente = null;
+    private DataInputStream input = null;
+    private DataOutputStream output = null;
+    
+    public Client_socket(String direccion, int puerto){
+        try{
+            cliente = new Socket(direccion, puerto);
+            System.out.println("conectado");
+            input = new DataInputStream(cliente.getInputStream());
+            output = new DataOutputStream(cliente.getOutputStream());            
+        }
+        catch(UnknownHostException e){
+            System.out.println(e);
+        }
+        catch(IOException i){
+            System.out.println(i);
+        }
+    }
+    
+    public void descargarArchivo() throws Exception{
+        InputStream in = cliente.getInputStream();
+        FileOutputStream out = new FileOutputStream("menu-cliente.xml");
+        int fileSize = 2048;
+        byte b[] = new byte[fileSize];
+        in.read(b, 0 ,b.length);
+        byte ar[]=new String(b).replaceAll("\0", "").getBytes();
+        out.write(ar, 0 ,ar.length);
+        cliente.close();
+    }
+    public void enviarArchivo() throws Exception{
+        FileInputStream file = new FileInputStream("menu-cliente.xml");
+        byte b[] = new byte[2048];
+        file.read(b, 0, b.length);
+        //DataOutputStream output = new DataOutputStream(cliente.getOutputStream());
+        output.writeInt(b.length);
+        output.write(b, 0, b.length);
+        cliente.close();
+    }
+    
+    public static void main(String[] args){       
+        Client_socket cliente = new Client_socket("127.0.0.1",8080);
+        try{
+            //cliente.descargarArchivo();
+            cliente.enviarArchivo();
+        }
+        catch(Exception e){
+            System.out.println("No se pudo descargar el archivo");
+        }
+        
+        
+    }
+    
+    
+}
---
+++ Server_socket.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iiiprogra;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author josca
+ */
+public class Server_socket {
+    
+    private ServerSocket server = null;
+    private Socket cliente = null;
+    
+    public Server_socket(int puerto){
+        try{
+            server = new ServerSocket(puerto);
+            cliente = server.accept(); 
+            FileInputStream file = new FileInputStream("menu.xml");
+            byte b[] = new byte[2048];
+            file.read(b, 0, b.length);
+            OutputStream output = cliente.getOutputStream();
+            output.write(b, 0, b.length);  
+            file.close();
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+        
+    }
+    
+    public void recibirArchivo() throws Exception{
+        DataInputStream input = new DataInputStream(new BufferedInputStream(cliente.getInputStream()));
+        FileOutputStream output1 = new FileOutputStream("menu.xml");
+        int length = input.readInt(); 
+        byte[] message = new byte[length];// read length of incoming message
+        if(length>0) {
+            input.readFully(message, 0, message.length); // read the message
+        }
+        byte ar[]=new String(message).replaceAll("\0", "").getBytes();
+        output1.write(ar, 0 ,ar.length);
+        output1.close();
+    }
+    
+    public static void main(String[] args) throws Exception{
+        Server_socket server = new Server_socket(8080);
+        server.recibirArchivo();
+    }
+}
