a2a1a388df7bedd5aab25cdaed7b6b8476af17fd
Yosua <yosuablancodiaz6@gmail.com>
2018-06-12 21:30:56 -0600

    table de orden casi terminada
    en la tabla de orden se montan los platillos deseados

--- ventanaPrincipalCliente.java
+++ ventanaPrincipalCliente.java
@@ -43,49 +43,46 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
 
         jDialog2 = new javax.swing.JDialog();
         jTabbedPane1 = new javax.swing.JTabbedPane();
-        PanePlatoFuerte = new javax.swing.JScrollPane();
-        tablaPlatosFuertes = new javax.swing.JTable();
         PanePostres = new javax.swing.JScrollPane();
         tablaPostres = new javax.swing.JTable();
         PaneBebidas = new javax.swing.JScrollPane();
         tablaBebidas = new javax.swing.JTable();
         PaneEntradas = new javax.swing.JScrollPane();
         tablaEntradas = new javax.swing.JTable();
+        PanePlatoFuerte = new javax.swing.JScrollPane();
+        tablaPlatosFuertes = new javax.swing.JTable();
         botonElimiar = new javax.swing.JButton();
-        ComboBox_tipo_envio = new javax.swing.JComboBox<>();
         jTabbedPane2 = new javax.swing.JTabbedPane();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tablaOrden = new javax.swing.JTable();
         botonAgregar = new javax.swing.JButton();
         botonEliminar = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        tablaPlatosFuertes.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-
-            },
-            new String [] {
-                "Nombre", "Calorias", "Piezas por porcion"
-            }
-        ));
-        PanePlatoFuerte.setViewportView(tablaPlatosFuertes);
-
-        jTabbedPane1.addTab("Platos Fuertes", PanePlatoFuerte);
 
         tablaPostres.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "Nombre", "Calorias", "Piezas por porcion"
+                "Nombre", "Calorias", "Piezas por porcion", "Precio"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         PanePostres.setViewportView(tablaPostres);
 
         jTabbedPane1.addTab("Postres", PanePostres);
@@ -95,9 +92,17 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Nombre", "Calorias", "Piezas por porcion"
+                "Nombre", "Calorias", "Piezas por porcion", "Precio"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, true
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         PaneBebidas.setViewportView(tablaBebidas);
 
         jTabbedPane1.addTab("Bebidas", PaneBebidas);
@@ -107,9 +112,17 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Nombre", "Calorias", "Piezas por porcion"
+                "Nombre", "Calorias", "Piezas por porcion", "Precio"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         tablaEntradas.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 tablaEntradasMouseClicked(evt);
@@ -119,41 +132,53 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("Entradas", PaneEntradas);
 
-        getContentPane().add(jTabbedPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 70, 420, 410));
-
-        botonElimiar.setText("Enviar");
-        botonElimiar.setMaximumSize(new java.awt.Dimension(126, 46));
-        getContentPane().add(botonElimiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 200, 150, -1));
-
-        ComboBox_tipo_envio.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Sitio", "Recoger", "Express" }));
-        ComboBox_tipo_envio.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ComboBox_tipo_envioActionPerformed(evt);
-            }
-        });
-        getContentPane().add(ComboBox_tipo_envio, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 80, 150, -1));
-
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tablaPlatosFuertes.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null}
+
             },
             new String [] {
-                "Platillo", "Cantidad", "Calorias"
+                "Nombre", "Calorias", "Piezas por porcion", "Precio"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false
+                false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
             }
         });
-        jScrollPane1.setViewportView(jTable1);
+        PanePlatoFuerte.setViewportView(tablaPlatosFuertes);
+
+        jTabbedPane1.addTab("Platos Fuertes", PanePlatoFuerte);
+
+        getContentPane().add(jTabbedPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 70, 420, 410));
+
+        botonElimiar.setText("Enviar");
+        botonElimiar.setMaximumSize(new java.awt.Dimension(126, 46));
+        getContentPane().add(botonElimiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 200, 150, -1));
+
+        tablaOrden.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Platillo", "Cantidad", "Calorias", "Precio"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tablaOrden);
 
         jTabbedPane2.addTab("Orden", jScrollPane1);
 
-        getContentPane().add(jTabbedPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 70, 260, 410));
+        getContentPane().add(jTabbedPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 70, 400, 410));
 
         botonAgregar.setText("Agregar a la Orden");
         botonAgregar.addActionListener(new java.awt.event.ActionListener() {
@@ -177,9 +202,15 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, -1, 20));
 
         jLabel2.setText("jLabel2");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 0, 50, 20));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 0, 50, 20));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 500, 260, 20));
+        jLabel3.setText("Precio total:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 80, -1));
+
+        jLabel4.setText("jLabel4");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 20, -1, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 500, 260, 50));
 
         fondo.setIcon(new javax.swing.ImageIcon("C:\\Users\\yosua\\Desktop\\fondo.jpg")); // NOI18N
         getContentPane().add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -192,35 +223,89 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
     }//GEN-LAST:event_tablaEntradasMouseClicked
 
     private void botonEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonEliminarActionPerformed
-        // TODO add your handling code here:
+        
+         
+        
+        
+        
+        
+        
     }//GEN-LAST:event_botonEliminarActionPerformed
 
     private void botonAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonAgregarActionPerformed
         
         int filaEntrada = tablaEntradas.getSelectedRow();
-        int filaPlatoFuerta = tablaPlatosFuertes.getSelectedRow();
+        int filaPlatoFuerte = tablaPlatosFuertes.getSelectedRow();
         int filaPostre = tablaPostres.getSelectedRow();
         int filaBebida = tablaBebidas.getSelectedRow();
         
         
         try{
-            if(filaEntrada == -1 && filaPlatoFuerta == -1 && filaPostre == -1 && filaBebida == -1){
+            if(filaEntrada == -1 && filaPlatoFuerte == -1 && filaPostre == -1 && filaBebida == -1){
                 JOptionPane.showMessageDialog(null, "Debe seleccionar un platillo","Advertencia", JOptionPane.WARNING_MESSAGE);
             }else{
                 String cantidadPorciones = JOptionPane.showInputDialog(null,"¿Cuantas porciones quieres?");
+                String nombre="";
+                String calorias="";
+                String precio="";
                 if(filaEntrada!= -1){
-                    String nombre = (String) tablaEntradas.getValueAt(filaEntrada, 0);
-                    String calorias = (String) tablaEntradas.getValueAt(filaEntrada, 1);
+                    System.out.println("entradaa");
+                     nombre = (String) tablaEntradas.getValueAt(filaEntrada, 0);
+                     calorias = (String) tablaEntradas.getValueAt(filaEntrada, 1);
+                     precio = (String) tablaEntradas.getValueAt(filaEntrada, 3);
+                     //int x = (Integer.parseInt(precio) * Integer.parseInt(cantidadPorciones)); // Esta variable guarda el precio * cantidad de porciones.
+                     //System.out.println(x);
+                     //String subtotal = x.toString();
+                     filaEntrada = -1;
+                     DefaultTableModel model = (DefaultTableModel) tablaOrden.getModel();
+                    String fila[] = {nombre,calorias,cantidadPorciones,precio};
+                    model.addRow(fila);
+                    tablaEntradas.clearSelection();
+                }else if(filaPlatoFuerte!= -1){
+                    System.out.println("platofuerte");
+                     nombre = (String) tablaPlatosFuertes.getValueAt(filaPlatoFuerte, 0);
+                     calorias = (String) tablaPlatosFuertes.getValueAt(filaPlatoFuerte, 1);
+                     precio = (String) tablaPlatosFuertes.getValueAt(filaPlatoFuerte, 3);
+                    // Integer x = (Integer.parseInt(precio) * Integer.parseInt(cantidadPorciones)); // Esta variable guarda el precio * cantidad de porciones.
+                     filaPlatoFuerte = -1;
+                     DefaultTableModel model = (DefaultTableModel) tablaOrden.getModel();
+                    String fila[] = {nombre,calorias,cantidadPorciones,precio};
+                    model.addRow(fila);
+                    tablaPlatosFuertes.clearSelection();
+                }else if(filaPostre!= -1){
+                    System.out.println("postre");
+                     nombre = (String) tablaPostres.getValueAt(filaPostre, 0);
+                     calorias = (String) tablaPostres.getValueAt(filaPostre, 1);
+                     precio = (String) tablaPostres.getValueAt(filaPostre, 3);
+                     //Integer x = (Integer.parseInt(precio) * Integer.parseInt(cantidadPorciones)); // Esta variable guarda el precio * cantidad de porciones.
+                     filaPostre =-1;
+                    DefaultTableModel model = (DefaultTableModel) tablaOrden.getModel();
+                    String fila[] = {nombre,calorias,cantidadPorciones,precio};
+                    model.addRow(fila);
+                    tablaPostres.clearSelection();
+                }else if(filaBebida!= -1){
+                    System.out.println("bebida");
+                     nombre = (String) tablaBebidas.getValueAt(filaBebida, 0);
+                     calorias = (String) tablaBebidas.getValueAt(filaBebida, 1);
+                     precio = (String) tablaBebidas.getValueAt(filaBebida, 3);
+                     //Integer x = (Integer.parseInt(precio) * Integer.parseInt(cantidadPorciones)); // Esta variable guarda el precio * cantidad de porciones.
+                     filaBebida = -1;
+                     DefaultTableModel model = (DefaultTableModel) tablaOrden.getModel();
+                    String fila[] = {nombre,calorias,cantidadPorciones,precio};
+                    model.addRow(fila);
+                    tablaBebidas.clearSelection();
                 }
+                
+                // Calculos 
+                
+                
+                
+                        
             }
         } catch(Exception e){
             
         }
     }//GEN-LAST:event_botonAgregarActionPerformed
-
-    private void ComboBox_tipo_envioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ComboBox_tipo_envioActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_ComboBox_tipo_envioActionPerformed
 
     
     
@@ -262,9 +347,9 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
     public void llenarTablaEntradas(ArrayList<String[]> matriz ){
         DefaultTableModel model = (DefaultTableModel) tablaEntradas.getModel();
         model.setRowCount(matriz.size());
-        model.setColumnCount(3);
+        model.setColumnCount(4);
         for(int i=0; i<matriz.size();i++){
-            for(int j=0; j<3;j++){
+            for(int j=0; j<4;j++){
                 tablaEntradas.setValueAt(matriz.get(i)[j], i, j);
             }
        }
@@ -273,9 +358,9 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
     public void llenarTablaPostres(ArrayList<String[]> matriz ){
         DefaultTableModel model = (DefaultTableModel) tablaPostres.getModel();
         model.setRowCount(matriz.size());
-        model.setColumnCount(3);
+        model.setColumnCount(4);
         for(int i=0; i<matriz.size();i++){
-            for(int j=0; j<3;j++){
+            for(int j=0; j<4;j++){
                 tablaPostres.setValueAt(matriz.get(i)[j], i, j);
             }
        }
@@ -284,9 +369,9 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
     public void llenarTablaPlatoFuerte(ArrayList<String[]> matriz ){
         DefaultTableModel model = (DefaultTableModel) tablaPlatosFuertes.getModel();
         model.setRowCount(matriz.size());
-        model.setColumnCount(3);
+        model.setColumnCount(4);
         for(int i=0; i<matriz.size();i++){
-            for(int j=0; j<3;j++){
+            for(int j=0; j<4;j++){
                 tablaPlatosFuertes.setValueAt(matriz.get(i)[j], i, j);
             }
        }
@@ -295,16 +380,15 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
     public void llenarTablaBebida(ArrayList<String[]> matriz ){
         DefaultTableModel model = (DefaultTableModel) tablaBebidas.getModel();
         model.setRowCount(matriz.size());
-        model.setColumnCount(3);
+        model.setColumnCount(4);
         for(int i=0; i<matriz.size();i++){
-            for(int j=0; j<3;j++){
+            for(int j=0; j<4;j++){
                 tablaBebidas.setValueAt(matriz.get(i)[j], i, j);
             }
        }
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<String> ComboBox_tipo_envio;
     private javax.swing.JScrollPane PaneBebidas;
     private javax.swing.JScrollPane PaneEntradas;
     private javax.swing.JScrollPane PanePlatoFuerte;
@@ -316,13 +400,15 @@ public class ventanaPrincipalCliente extends javax.swing.JFrame {
     private javax.swing.JDialog jDialog2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTabbedPane jTabbedPane2;
-    private javax.swing.JTable jTable1;
     private javax.swing.JTable tablaBebidas;
     private javax.swing.JTable tablaEntradas;
+    private javax.swing.JTable tablaOrden;
     private javax.swing.JTable tablaPlatosFuertes;
     private javax.swing.JTable tablaPostres;
     // End of variables declaration//GEN-END:variables
